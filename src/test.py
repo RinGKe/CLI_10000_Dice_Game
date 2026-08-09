6a01aeff01ef4ba6aaeee025c60f200d2edf60d1
RinGke <keaganspires@gmail.com>
2026-08-09 23:10:26 +0000

    Test updates and added cpu.py

--- src/test.py
+++ src/test.py
@@ -17,38 +17,50 @@ def test_combos(nums=[]) -> None:
     print("=======================")
 
 
-def test_multi_roll(nums=[]) -> None:
+def test_machine():
+    iteration = 0
+    total_score = 1000
+    current_score = 0
+    while True:
+        iteration += 1
+        current_score = test_multi_roll()
+        total_score += current_score
+        average = round(total_score / iteration)
+        print(f"**** TARGET: {average} ****")
+        if current_score > 10000:
+            break
+        if iteration == 10:
+            iteration = 0
+            total_score = 1000
+        time.sleep(0.5)
+
+
+def test_multi_roll(nums=[]) -> int:
     dice = Dice("CPU")
-    roll = nums if nums else dice.roll_dice()
-    print(roll)
-    data = dice.check_selection(roll)
-    if not data:
-        print("BUST")
-    for k, v in data.items():
-        print(f"{k}: {v}")
-    print("-----------------------")
-    print(f"Score: {dice.current_score}")
-    print("=======================")
-    while data:
-        time.sleep(1)
+    while True:
+        time.sleep(0.5)
         roll = dice.roll_dice()
         if len(roll) == 6:
-            print("=======================")
+            print("=================================")
             print(f"Cup: {dice.cup_num}")
         print()
         print(roll)
         data = dice.check_selection(roll)
+        if not data:
+            break
         for k, v in data.items():
             print(f"{k}: {v}")
         print("-----------------------")
         print(f"Score: {dice.current_score}")
-        print("=======================")
+        print("=========================")
     print("BUST")
-    print("-----------------------")
+    print("-------------------------")
     print(dice)
+    return dice.current_score
 
 
-test_multi_roll()
+test_machine()
+
 
 """
 test_combos([1, 2, 3, 4, 5, 6])
